6464f6c35f09d1bad6f6b687554d1bb4e229c13f
donquix <wordany@yahoo.co.uk>
2015-07-29 08:54:29 +0100

    json from file test

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
 
 
 var xmlhttp = new XMLHttpRequest();
-//var url = "my.txt";
-var url = "http://www.w3schools.com/website/customers_mysql.php";
+var url = "https://github.com/donquix/current/blob/gh-pages/my.txt";
+//var url = "http://www.w3schools.com/website/customers_mysql.php";
 
 xmlhttp.onreadystatechange = function() {
 	if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
 		var arr = JSON.parse(xmlhttp.responseText);
-		//myFunction(arr);
-		myFunction1(arr);
+		myFunction(arr);
+		//myFunction1(arr);
 	}
 }
 xmlhttp.open("GET", url, true);
